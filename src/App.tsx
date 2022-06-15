43cceebc24a75c0259bb316a4cdeadbadd072ee0
Luan Ferreira Campos <lfcampos.eab@gmail.com>
2022-06-15 20:32:33 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,16 @@ const App = () => {
       </header>
       <section className={styles.container}>
         <div className={styles.leftSide}>
-          Esquerda
+          <h1>Calcule o seu IMC</h1>
+          <p>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
+          <input
+            type="number"
+            placeholder='Digite a sua altura (cm)'
+            value={}
+            onChange={}
+          />
         </div>
+
         <div className={styles.rightSide}>
           Direita
         </div>
